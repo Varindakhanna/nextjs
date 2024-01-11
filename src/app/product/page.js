50a79ebc0a466603ea1aa7a38141f02c4c971b1f
VarindaKhanna <varindakhanna2002@gmail.com>
2024-01-11 15:43:35 +0530

    server and client component used together

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -1,27 +1,53 @@
+import Price from "./price";
 // fetching api from client side component
 
-"use client"
-import { useEffect, useState } from "react";
+// "use client"
+// import { useEffect, useState } from "react";
 
-export default function Product() {
-  const [product, setProduct] = useState([]);
+// export default function Product() {
+//   const [product, setProduct] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      let data = await fetch("https://dummyjson.com/products");
-      data = await data.json();
-      setProduct(data.products);
-    }
-    fetchData();
-  }, []);
+//   useEffect(() => {
+//     async function fetchData() {
+//       let data = await fetch("https://dummyjson.com/products");
+//       data = await data.json();
+//       setProduct(data.products);
+//     }
+//     fetchData();
+//   }, []);
 
+//   return (
+//     <div>
+//       <h1>Product List</h1>
+//       {product.map((item) => (
+//         <h3>{item.title}</h3>
+//       ))}
+//     </div>
+//   );
+// }
+
+// fetching api from server side component
+async function productList() {
+  let data = await fetch("https://dummyjson.com/products");
+  data = await data.json();
+  return data.products;
+}
+export default async function Product() {
+  let product = await productList();
+  //console.log("===");
   return (
     <div>
       <h1>Product List</h1>
+      <div></div>
       {product.map((item) => (
-        <h3>{item.title}</h3>
-      ))}
+
+       <div>
+         <h3>{item.title}</h3>
+         <Price price={item.price}/>
+       </div>
+       
+         
+))}
     </div>
   );
 }
-
---
+++ src/app/product/price.js
@@ -0,0 +1,11 @@
+"use client"
+            
+export default function Price({price})
+{
+    return(
+        <button onClick={()=>{alert(price)}}>Click me</button>
+
+        
+        
+    )
+}
