635305f2c39904efdecbcda476aec7885991264d
VarindaKhanna <varindakhanna2002@gmail.com>
2024-01-12 11:50:18 +0530

    changed metadata

---
+++ src/app/generatemetadata/page.js
@@ -0,0 +1,14 @@
+export default function GMD()
+{
+    return(
+        <div>Changing meta data</div>
+    )
+}
+
+export function generateMetadata()
+{
+    return{
+        title:"changed title",
+        description:"its changed bro"
+    }
+}
